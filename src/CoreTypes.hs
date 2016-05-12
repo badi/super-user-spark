09dda0f975ee4c12a955caab3d5fb7e77d315d5e
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2016-05-11 23:08:36 -0400

    WIP add PipeDeployment to DeploymendKind

--- src/CoreTypes.hs
+++ src/CoreTypes.hs
@@ -2,26 +2,41 @@
 module CoreTypes where
 
 import           Control.Monad (mzero)
-import           Data.Aeson    (FromJSON (..), ToJSON (..), Value (..))
+import           Data.Aeson    (FromJSON (..), ToJSON (..), Value (..), object,
+                                (.:), (.=))
+import           Data.Text     (Text)
 
 type Directory = FilePath
+
+-- | A 'Cmd' is a pair of (path to executable, arguments).
+--
+-- The path to the executable and either be bare (eg "file") in which
+-- case it is found by inspecting the `$PATH`, or the absolute path to
+-- the executable.
+type Cmd = (FilePath, [Text])
+
+-- | A 'Pipe' is a sequence of 'Cmd's that are applied sequentially
+type Pipe = [Cmd]
 
 -- | The kind of a deployment
 data DeploymentKind = LinkDeployment
                     | CopyDeployment
+                    | PipeDeployment Pipe
     deriving (Show, Eq)
 
 instance Read DeploymentKind where
     readsPrec _ "link" = [(LinkDeployment,"")]
     readsPrec _ "copy" = [(CopyDeployment,"")]
-    readsPrec _ _ = []
+    readsPrec _ _ = undefined -- FIXME Read DeploymentKind
 
 instance FromJSON DeploymentKind where
     parseJSON (String "link") = return LinkDeployment
     parseJSON (String "copy") = return CopyDeployment
+    parseJSON (Object o) = PipeDeployment <$> o .: "pipe"
     parseJSON _ = mzero
 
 instance ToJSON DeploymentKind where
     toJSON LinkDeployment = String "link"
     toJSON CopyDeployment = String "copy"
+    toJSON (PipeDeployment cmd) = object [ "pipe" .= cmd ]
 
